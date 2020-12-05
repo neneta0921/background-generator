139cc3a97a472d50c59b574aef4703bee3027785
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-05 17:56:12 +0900

    adding starting porject

---
+++ script.js
@@ -0,0 +1,32 @@
+const body = document.getElementById('gradient');
+const css = document.querySelector('h3');
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+const randomBtn = document.getElementById('random');
+
+function showCurrentColor() {
+  document.body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  css.textContent = body.style.background + ';';
+}
+
+function createRandomColor() {
+  let randomColor = '#';
+  for (let i = 0; i < 6; i++) {
+    randomColor += ((16 * Math.random()) | 0).toString(16);
+  }
+  return randomColor;
+}
+
+function setRandomColor() {
+  color1.value = createRandomColor();
+  color2.value = createRandomColor();
+  showCurrentColor();
+}
+
+// EventListener
+color1.addEventListener('input', showCurrentColor);
+color2.addEventListener('input', showCurrentColor);
+randomBtn.addEventListener('click', setRandomColor);
+
+// On Load
+showCurrentColor();
