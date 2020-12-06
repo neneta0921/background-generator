c24e6734a57f751d0e53626d746fc1bca75b0812
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2020-12-07 07:33:51 +0900

    csstyle output to input-text

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const _ = require('lodash');
-
 const body = document.getElementById('gradient');
 const css = document.querySelector('h3');
 const color1 = document.querySelector('.color1');
 const color2 = document.querySelector('.color2');
 const randomBtn = document.getElementById('random');
-const cssInfo = document.getElementById('css-info');
+const cssInfo = document.getElementById('target');
 
 function showCurrentColor() {
-  document.body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
-  css.textContent = body.style.background + ';';
+  let backgroundStyle = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+  document.body.style.background = backgroundStyle;
+  css.textContent = `${backgroundStyle};`;
+  cssInfo.value = `${backgroundStyle};`;
 }
 
 function createRandomColor() {
